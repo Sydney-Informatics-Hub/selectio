b09c54853c09c102a7068c0059bfd07e1d4279d1
Seb Haan <sebhaan@gmail.com>
2022-11-01 13:45:01 +1100

    Add max_samples, n_jobs, and rm correlation option

--- selectio/models/rf.py
+++ selectio/models/rf.py
@@ -5,46 +5,67 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.inspection import permutation_importance
 from scipy.stats import spearmanr
+import multiprocessing
 
 __name__ = 'RF'
 __fullname__ = 'Random Forest Permutation' 
 
-def factor_importance(X_train, y_train, correlated = False, norm = True):
+
+def set_njobs():
+	"""
+	Set optimal number of parallel jobs. 
+	Here: N_Jobs = N_CPU - 1
+	"""
+	ncpu = multiprocessing.cpu_count()
+	if ncpu > 1:
+		njobs = ncpu - 1
+	else:
+		njobs = 1
+	return njobs
+
+
+
+def factor_importance(X_train, y_train, norm = True, max_samples = 5000):
 	"""
 	Factor importance using RF permutation test and optional corrections 
 	for multi-collinarity (correlated) features. 
 	Including training of Random Forest regression model with training data 
 	and setting non-significant coefficients to zero.
 
+	Note that for large datasets, it is recommended to set max_samples. 
+	This option may provide less accurate importance estimates, but
+    it keeps the method tractable.
+
 	Input:
 		X: input data matrix with shape (npoints,nfeatures)
 		y: target varable with shape (npoints)
-		correlated: if True, features are assumed to be correlated
 		norm: boolean, if True (default) normalize correlation coefficients to sum = 1
+		max_samples: The maximum number of samples to draw from X in each repeat (without replacement)
 
 	Return:
 		imp_mean_corr: feature importances
 	"""
+
+	n_jobs = set_njobs()
+	if len(y_train) < max_samples:
+		max_samples = 1.0
+	
 	rf_reg = RandomForestRegressor(n_estimators=500, min_samples_leaf=4, random_state = 42)
 	rf_reg.fit(X_train, y_train)
-	result = permutation_importance(rf_reg, X_train, y_train, n_repeats=20, random_state=42, 
-		n_jobs=1, scoring = "neg_mean_squared_error")
-	imp_mean = result.importances_mean
-	imp_std = result.importances_std
-	# Make corrections for correlated features
-	# This is necessary since permutation importance are lower for correlated features
-	if correlated:
-		corr = spearmanr(X_train).correlation
-		imp_mean_corr = np.zeros(len(imp_mean))
-		imp_std_corr = np.zeros(len(imp_mean))
-		for i in range(len(imp_mean)):
-			imp_mean_corr[i] = np.sum(abs(corr[i]) * imp_mean)
-			imp_std_corr[i] = np.sqrt(np.sum(abs(corr[i]) * imp_std**2))
-	else:
-		imp_mean_corr = imp_mean
-		imp_std_corr = imp_std
-	#print("Random Forest factor importances: ", imp_mean_corr)
-	#print("Random Forest factor importances std: ", imp_std_corr)
+	
+	result = permutation_importance(
+		rf_reg, 
+		X_train, 
+		y_train, 
+		n_repeats=20, 
+		random_state=42, 
+		n_jobs=n_jobs, 
+		scoring = "neg_mean_squared_error", 
+		max_samples = max_samples)
+
+	imp_mean_corr = result.importances_mean
+	imp_std_corr = result.importances_std
+
 	# Set non significant features to zero:
 	imp_mean_corr[imp_mean_corr / imp_std_corr < 3] = 0
 	imp_mean_corr[imp_mean_corr < 0.001] = 0
