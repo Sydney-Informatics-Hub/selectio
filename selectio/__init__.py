3ccbeaae6b34578c0cf7749fa88dd4a30c2c8406
Seb Haan <sebhaan@gmail.com>
2022-11-18 12:43:33 +1100

    Update docs

--- selectio/__init__.py
+++ selectio/__init__.py
@@ -2,10 +2,10 @@
 Multi-model Feature Importance Scoring and auto Feature Selection.
 ------------------------------------------------------------------- 
 
-This Python package returns multiple feature importance scores, feature ranks,
-and automatically suggests a feature selection based on the majority vote of all models.
+This Python package returns multiple feature importance scores and automatically suggests a 
+feature selection based on the majority vote of all models.
 
-Currently the following six models for feature importance scoring are included:
+The following six models for feature importance scoring are included:
 - Spearman rank analysis (see 'models.spearman')
 - Correlation coefficient significance of linear/log-scaled Bayesian Linear Regression (see 'models.blr')
 - Random Forest Permutation test (see 'models.rf.py')
@@ -13,8 +13,8 @@ Currently the following six models for feature importance scoring are included:
 - Mutual Information Regression (see 'models.mi')
 - General correlation coefficients (see 'models.xicor')
 
-The feature selection score can be either computed directly using the class Fsel, or can be called directly 
-with more functionality using a settings yaml file:
+The feature selection score can be either computed directly using the class selectio.Fsel, 
+or can be called directly with more functionality using a settings yaml file:
 
 python selectio.py -s fname_settings
 
@@ -24,7 +24,16 @@ Alternatively, the settings file can be specified as a command line argument wit
 '-s', or '--settings' followed by PATH-TO-FILE/FILENAME.yaml 
 (e.g. python selectio.py -s settings/settings_featureimportance.yaml).
 
+The current feature importance models support numerical data only. 
+Categorical data will need to be encoded to numerical features beforehand.
+
+All model scores are normalized to unity, i.e., $\sum _i^{N_{features}} score_i = 1$
+	
+Feature-to-feature correlations are automatically clustered using hierarchical clustering of the 
+Spearman correlation coefficients (for more details see `utils.plot_feature_correlation_spearman`).
+
 For more details see: https://github.com/sebhaan/selectio
+and the companion example notebook in the 'notebooks' folder.
 """
 
 __version__ = "0.1.7"
